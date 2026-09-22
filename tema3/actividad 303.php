e8e74a46c4c439dc75c408d7f401132018490664
Edurne <edurneruizdemiguel@gmail.com>
2026-09-22 16:53:55 +0200

    22/9 casa

--- tema3/actividad 303.php
+++ tema3/actividad 303.php
@@ -1,11 +1,11 @@
 <?php 
 
-$extension = 'png';
+$extension = 'webp';
 
 $resultado = match ($extension) {
     'jpg','jpeg', 'png', 'webp' => 'icon-media',
     'pdf', 'docx', 'txt' => 'icon-doc',
-    
+    'php', 'js', 'html', 'css' => 'icon-code',
     default  => 'icon-file',
 };
 
